31267f56c4ce4da816d895e793d7af59a6ad97a6
amar-hiremath <amarhiremath49@gmail.com>
2024-03-05 13:17:46 +0530

    all together 3 (three) cards added to the page| updated data.jsx and added ogCard.jsx for further refrence.

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -3,7 +3,6 @@ const cardsData = [
     tag: {
       present: true,
       title: "Best Choice",
-      
     },
     img: "src/components/images/img.png",
     name: "Builder 1",
@@ -14,21 +13,94 @@ const cardsData = [
       heading: "Main highlights",
       description:
         "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
-      rating: {
-        num: 3.8,
-        quality: "Exceptional",
-      },
-      answers: ["Sleek design", "Advanced features", "Great customer support"],
+      answers: null,
     },
     rating: {
       num: 9.8,
       quality: "Exceptional",
       icon: true,
     },
-    viewLink: "/productB",
-    moreData: true ,
+    viewLink: "/",
+    moreData: true,
+  },
+  {
+    tag: {
+      present: true,
+      title: "Best Value",
+    },
+    img: "src/components/images/img.png",
+    name: "Biulder",
+    productName: "SiteCraft 68-Inch Ultimate Web Design Studio- ",
+    productDescription:
+      "Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
+    highlights: {
+      heading: "Main highlights",
+      description:
+        "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
+    },
+    rating: {
+      num: 9.5,
+      quality: "Excellent",
+      icon: true,
+    },
+    viewLink: "/",
+    moreData: true,
+  },
+  {
+    tag: {
+      present: false,
+      title: "null",
+    },
+    img: "src/components/images/img.png",
+    name: "Builder 1",
+    productName: "WixPro 72-Inch Responsive Website Builder- ",
+    productDescription:
+      "Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+    highlights: {
+      heading: "Main highlights",
+      description:
+        "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+    },
+    rating: {
+      num: 9.3,
+      quality: "Exceptional",
+      icon: false,
+    },
+    viewLink: "/",
+    moreData: true,
   },
   // Add more card data objects as needed
 ];
 
 export default cardsData;
+
+
+// {
+//   tag: {
+//     present: true,
+//     title: "Best Choice",
+    
+//   },
+//   img: "src/components/images/img.png",
+//   name: "Builder 1",
+//   productName: "WixPro 72-Inch Responsive Website Builder-",
+//   productDescription:
+//     "Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+//   highlights: {
+//     heading: "Main highlights",
+//     description:
+//       "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+//     rating: {
+//       num: 3.8,
+//       quality: "Exceptional",
+//     },
+//     answers: ["Sleek design", "Advanced features", "Great customer support"],
+//   },
+//   rating: {
+//     num: 9.8,
+//     quality: "Exceptional",
+//     icon: true,
+//   },
+//   viewLink: "/productB",
+//   moreData: true ,
+// },
---
+++ src/components/data/ogCard.jsx
@@ -0,0 +1,37 @@
+
+
+  const cardsData = [
+    {
+      tag: {
+        present: true,
+        title: "Best Choice",
+        
+      },
+      img: "src/components/images/img.png",
+      name: "Builder 1",
+      productName: "WixPro 72-Inch Responsive Website Builder-",
+      productDescription:
+        "Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+      highlights: {
+        heading: "Main highlights",
+        description:
+          "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+        rating: {
+          num: 3.8,
+          quality: "Exceptional",
+        },
+        answers: ["Sleek design", "Advanced features", "Great customer support"],
+      },
+      rating: {
+        num: 9.8,
+        quality: "Exceptional",
+        icon: true,
+      },
+      viewLink: "/productB",
+      moreData: true ,
+    },
+    // Add more card data objects as needed
+  ];
+  
+  export default cardsData;
+  
